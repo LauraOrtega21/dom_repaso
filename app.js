4f6dfa34501ef59ea873bf437cd79e4f19dab98e
Laura Ortega <laurapaolao2110@gmail.com>
2024-06-19 10:12:58 -0500

    Primer

---
+++ app.js
@@ -0,0 +1,12 @@
+//objeto padre en el navegador
+let win = window;
+let dom = document;
+let head= dom.head;
+let body = dom.body;
+
+
+
+console.log(window)
+console.log(dom)
+console.log(head)
+console.log(body)
